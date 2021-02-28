7c266c39e902d62f70c96747f357cd404b30f30b
Miron Lončarić <miron.loncaric@gmail.com>
2021-02-28 08:26:00 +0100

    Adding deleted code...

--- client/src/Components/Entries.js
+++ client/src/Components/Entries.js
@@ -1,2 +1,37 @@
+import React, { useEffect, useState } from 'react'
+import Comments from './Comments'
+
+import './Entries.css'
+import Entry from './Entry'
+
+export default function Entries({ entries }) {
+
+    const [rerender, setRerender] = useState();
+
+    useEffect(() => {
+        setRerender(rerender => rerender+1);
+    }, [entries]);
+
+    return (
+        <div>
+            { (entries.length>0) ?
+                entries.map(({ postBody, uname, hashtags, emoji, date, uid, _id }, id) => (
+                    <div key={ id }>
+                        <Entry
+                            date={ date }
+                            content={ postBody }
+                            emoji={ emoji }
+                            hashtags={ hashtags }
+                            uname={ uname }
+                            id={ _id }
+                            uid={ uid }
+                            rerender={rerender}
+                        />
+                    </div>
+                ))
+                :
+                <h4 className="no-posts">No posts yet... 😕</h4>
             }
         </div>
+    );
+}
